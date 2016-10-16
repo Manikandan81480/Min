80eee64924151c7b2d4b7f65d78607bd48fa393f
Manikandan81480 <mani81480@gmail.com>
2016-10-16 04:04:35 -0700

    Update and rename README.md to Min.java

---
+++ Min.java
@@ -0,0 +1,17 @@
+class Min
+{
+public static void main(String... args)
+{
+Scanner x=ne Scanner(System.in);
+int x=x.nextInt();
+int y=x.nextInt();
+if(x<y)
+{
+System.out.println("x is small"+x);
+}
+else
+{
+System.out.println("y is small"+y);
+}
+}
+}
